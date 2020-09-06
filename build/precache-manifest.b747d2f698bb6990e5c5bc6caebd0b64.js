80097b9a613bdbc4f3b83d0a9faa06e4ca7f0a09
Muhammad Shiraz <61803642+MuhmmadSheraz@users.noreply.github.com>
2020-09-06 20:54:21 +0500

    company component added

---
+++ build/precache-manifest.b747d2f698bb6990e5c5bc6caebd0b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7640f3d829723c0888517006d5d57b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f6797df23a0a0e8b3ff",
+    "url": "/static/css/main.7efea952.chunk.css"
+  },
+  {
+    "revision": "767a33142325d2a9adb7",
+    "url": "/static/js/2.daf1abcf.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.daf1abcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6797df23a0a0e8b3ff",
+    "url": "/static/js/main.6364d9bf.chunk.js"
+  },
+  {
+    "revision": "7d3bb73ac49ec751a2cd",
+    "url": "/static/js/runtime-main.ab8a042e.js"
+  }
+]);
